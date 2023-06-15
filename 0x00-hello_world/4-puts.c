d4125b23fab2e30d5c0652aa25c05f6e0899752e
hanad2 <hanad252@gmail.com>
2023-06-15 11:46:41 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the desired string.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+	return (0);
 }
+
