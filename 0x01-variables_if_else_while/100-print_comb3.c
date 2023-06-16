de7c94e254e458a280a8c62989d5ee319387525f
hanad2 <hanad252@gmail.com>
2023-06-16 14:23:16 -0700

    fixed 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,28 +2,29 @@
 
 /**
  * main - prints all possible different combinations of two digits
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int n, m;
+	int n, m;
 
-        for (n = 0; n <= 9; n++)
-        {
-                for (m = n + 1; m <= 9; m++)
-                {
-                        putchar(n + '0');
-                        putchar(m + '0');
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = n + 1; m <= 9; m++)
+		{
+			putchar(n + '0');
+			putchar(m + '0');
 
-                        if (n != 8 || m != 9)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                }
-        }
+			if (n != 8 || m != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
 
