a3a2b777cc9edaf707d8d608c069a29792d7037c
hanad2 <hanad252@gmail.com>
2023-06-20 16:57:21 -0700

    fixed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,7 +17,10 @@ void print_times_table(int n)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			printf("%d\t", i * j);
+			if (j != 0)
+				printf(", ");
+
+			printf("%-4d", i * j);
 		}
 		printf("\n");
 	}
