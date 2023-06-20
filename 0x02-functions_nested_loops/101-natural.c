86f2ce3c95de0519c5eb4b3e70844f8767e0df67
hanad2 <hanad252@gmail.com>
2023-06-20 16:44:20 -0700

    fixed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
-int sum_multiples_3_or_5() {
-    int total = 0;
-    int num;
-    
-    for (num = 1; num < 1024; num++) {
-        if (num % 3 == 0 || num % 5 == 0) {
-            total += num;
-        }
-    }
-    
-    return total;
+int sum_multiples_3_or_5(void)
+{
+	int total = 0;
+	int num;
+
+	for (num = 1; num < 1024; num++)
+	{
+		if (num % 3 == 0 || num % 5 == 0)
+		{
+			total += num;
+		}
+	}
+
+	return total;
 }
 
-int main() {
-    int result = sum_multiples_3_or_5();
-    printf("%d\n", result);
-    
-    return (0);
+int main(void)
+{
+	int result = sum_multiples_3_or_5();
+	printf("%d\n", result);
+
+	return (0);
 }
 
