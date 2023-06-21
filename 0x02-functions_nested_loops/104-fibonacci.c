dfa5067977e856e4aa82c5f85653e6ff11a060f6
hanad2 <hanad252@gmail.com>
2023-06-21 11:36:50 -0700

    fixed 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,46 +1,26 @@
 #include <stdio.h>
 
-void print_fibonacci(void);
-void add(unsigned int *num1, unsigned int *num2, unsigned int *result);
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    print_fibonacci();
-
-    return 0;
-}
-
-void print_fibonacci(void)
-{
-    unsigned int a[100] = {1, 2};
-    unsigned int *prev = &a[0];
-    unsigned int *curr = &a[1];
-    unsigned int result[100];
     int i;
+    unsigned long long int fib1 = 1, fib2 = 1, fib_sum;
 
-    printf("%u, %u", *prev, *curr);
+    printf("%llu, %llu", fib1, fib2);
 
-    for (i = 2; i < 98; i++)
+    for (i = 2; i < 100; i++)
     {
-        add(prev, curr, result);
-        printf(", %u", *result);
-
-        prev = curr;
-        curr = result;
+        fib_sum = fib1 + fib2;
+        printf(", %llu", fib_sum);
+        fib1 = fib2;
+        fib2 = fib_sum;
     }
 
     printf("\n");
-}
-
-void add(unsigned int *num1, unsigned int *num2, unsigned int *result)
-{
-    int i, carry = 0;
-
-    for (i = 0; i < 100; i++)
-    {
-        int sum = num1[i] + num2[i] + carry;
-        result[i] = sum % 10;
-        carry = sum / 10;
-    }
+    return 0;
 }
 
