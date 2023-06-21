a277ab88a6dfc66c6b61406de6ee50021695d6a4
hanad2 <hanad252@gmail.com>
2023-06-21 13:53:35 -0700

    finished

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Tests the positive_or_negative() function with a case of 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+
